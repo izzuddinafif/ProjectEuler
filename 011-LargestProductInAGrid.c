7bbea4523d845d0cb7793efccdae88e041edc675
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2023-01-02 15:01:49 +0700

    .

--- 011-LargestProductInAGrid.c
+++ 011-LargestProductInAGrid.c
@@ -1,77 +1,69 @@
 #include<stdio.h>
-int d[20][20], temp=1; x=0;
+int d[400], temp=1; x=0;
 void inputData(void)
 {
-    for(int i=0; i<20; i++)
+    printf("Copy and paste the 20x20 grid from the website here:\n");
+    for(int i=0; i<400; i+=20)
     {
-        scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &d[i][1], &d[i][2], &d[i][3], &d[i][4], &d[i][5], &d[i][6], &d[i][7], &d[i][8], &d[i][9], &d[i][10], &d[i][11], &d[i][12], &d[i][13], &d[i][14], &d[i][15], &d[i][16], &d[i][17], &d[i][18], &d[i][19], &d[i][20]);
-    }  
+        scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &d[i], &d[i+1], &d[i+2], &d[i+3], &d[i+4], &d[i+5], &d[i+6], &d[i+7], &d[i+8], &d[i+9], &d[i+10], &d[i+11], &d[i+12], &d[i+13], &d[i+14], &d[i+15], &d[i+16], &d[i+17], &d[i+18], &d[i+19]);
+    }
 }
 void gridMulti(void)
 {
-    for(int i=0; i<20; i++)
+    for(int i=0; i<400; i++)
     {
-        for(int j=0; j<20; j++)
+        if(i>=0 && i<400-3) // left & right
         {
-            if(j-4>=0 && j<=20) // left & right
+            for(int k=i; k<=i+3; k++)
             {
-                for(int k=j-4; k<j; k++)
-                {
-                    temp*=d[i][k];
-                }
-                if(temp>x)
-                    x=temp;
-                temp=1;
+                temp*=d[k];
             }
-            if(i-4>=0 && i<=20) // up & down
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+        if(i+60<400) // up & down
+        {
+            for(int k=i; k<=i+60; k+=20)
             {
-                for(int k=i-4; k<i; k++)
-                {
-                    temp*=d[k][j];
-                }
-                if(temp>x)
-                    x=temp;
-                temp=1;
+                temp*=d[k];
             }
-            if(i+4<=20 && j+4<=20) // diagonal up-down right
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+        if(i+63<400) // diagonal up-down right
+        {
+            for(int k=i; k<=i+63; k+=21)
             {
-                int k=i, l=j;
-                while(k<i+4 && l<j+4)
-                {
-                    temp*=d[k][l];
-                    k++;
-                    l++;   
-                }
-                if(temp>x)
-                    x=temp;
-                temp=1;
+                temp*=d[k];
             }
-            if(i-4<=0 && j-4<=0) // diagonal up-down left
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+        if(i+57<400) // diagonal up-down left
+        {
+            for(int k=i; k<=i+57; k+=19)
             {
-                int k=i, l=j;
-                while(k>i-4 && l>j-4)
-                {
-                    temp*=d[k][l];
-                    k--;
-                    l--;
-                }
-                if(temp>x)
-                    x=temp;
-                temp=1;
+                temp*=d[k];
             }
+            if(temp>x)
+                x=temp;
+            temp=1;
         }
     }
-
-}
+    } 
 int main(void)
 {
     inputData();
     gridMulti();
     puts("");
-    for(int i=0; i<20; i++)
+    /* for(int i=0; i<400; i+=20)
     {
-        printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", d[i][1], d[i][2], d[i][3], d[i][4], d[i][5], d[i][6], d[i][7], d[i][8], d[i][9], d[i][10], d[i][11], d[i][12], d[i][13], d[i][14], d[i][15], d[i][16], d[i][17], d[i][18], d[i][19], d[i][20]);
-    }  
-    puts("");
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", d[i], d[i+1], d[i+2], d[i+3], d[i+4], d[i+5], d[i+6], d[i+7], d[i+8], d[i+9], d[i+10], d[i+11], d[i+12], d[i+13], d[i+14], d[i+15], d[i+16], d[i+17], d[i+18], d[i+19]);
+    }
+    puts(""); */
     printf("%d", x);
 }
+//ans 70600674
--- test.c
+++ test.c
@@ -1,28 +1,68 @@
 #include<stdio.h>
-#include<math.h>
-int isPrime(int y)
+int d[400], temp=1; x=0;
+void inputData(void)
 {
-    for(int i=2; i<sqrt(y); i++)
+    for(int i=0; i<400; i+=20)
     {
-        if(y%i==0)
-        {
-            return 0;
-        }
-    }   
-    return 1;
+        scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &d[i], &d[i+1], &d[i+2], &d[i+3], &d[i+4], &d[i+5], &d[i+6], &d[i+7], &d[i+8], &d[i+9], &d[i+10], &d[i+11], &d[i+12], &d[i+13], &d[i+14], &d[i+15], &d[i+16], &d[i+17], &d[i+18], &d[i+19]);
+    }
 }
+void gridMulti(void)
+{
+    for(int i=0; i<400; i++)
+    {
+        if(i>=0 && i<400-3) // left & right
+        {
+            for(int k=i; k<=i+3; k++)
+            {
+                temp*=d[k];
+            }
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+        if(i+60<400) // up & down
+        {
+            for(int k=i; k<=i+60; k+=20)
+            {
+                temp*=d[k];
+            }
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+        if(i+63<400) // diagonal up-down right
+        {
+            for(int k=i; k<=i+63; k+=21)
+            {
+                temp*=d[k];
+            }
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+        if(i+57<400) // diagonal up-down left
+        {
+            for(int k=i; k<=i+57; k+=19)
+            {
+                temp*=d[k];
+            }
+            if(temp>x)
+                x=temp;
+            temp=1;
+        }
+    }
+    } 
 int main(void)
 {
-    int n=2, x;
-    long long sum=0;
-    while(n<=2000000)
-    { 
-        if(isPrime(n)==1)
-        {
-            sum+=n; 
-            printf("%d\n", n);
-        }
-        n++; 
+    inputData();
+    gridMulti();
+    puts("");
+    for(int i=0; i<400; i+=20)
+    {
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", d[i], d[i+1], d[i+2], d[i+3], d[i+4], d[i+5], d[i+6], d[i+7], d[i+8], d[i+9], d[i+10], d[i+11], d[i+12], d[i+13], d[i+14], d[i+15], d[i+16], d[i+17], d[i+18], d[i+19]);
     }
-    printf("%lld\n", sum);
+    puts("");
+    printf("%d", x);
 }
+//ans 70600674
